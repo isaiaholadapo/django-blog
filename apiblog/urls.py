3645040a721f4a311944952dbebc7a2b953b4413
Isaiah <isaiaholadapo18@gmail.com>
2021-01-28 01:07:26 +0100

    added blogpost creation form

--- apiblog/urls.py
+++ apiblog/urls.py
@@ -14,23 +14,29 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from personal.views import home_screen_view
 from account.views import (
     registration_view,
     logout_view,
     login_view,
     account_view,
+    must_authenticate_view,
 )
 from django.contrib.auth import views as auth_views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls', 'blog')),
     path('', home_screen_view, name = 'home'),
     path('register/', registration_view, name = 'register'),
     path('logout/', logout_view, name = 'logout'),
     path('login/', login_view, name = 'login'),
     path('account/', account_view, name = 'account'),
+    path('must_authenticate/', must_authenticate_view, name = 'must_authenticate'),
 
     # Password reset links (ref: https://github.com/django/django/blob/master/django/contrib/auth/views.py)
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), 
@@ -48,3 +54,7 @@ urlpatterns = [
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
      name='password_reset_complete'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_URL)
