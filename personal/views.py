e37adee3dfdc8040360937374f437c7c03d267c9
Isaiah <isaiaholadapo18@gmail.com>
2021-02-02 11:13:02 +0100

    added search bar

--- personal/views.py
+++ personal/views.py
@@ -1,11 +1,17 @@
 
 from django.shortcuts import render
-from account.models import Account
+from blog.models import BlogPost
+from operator import attrgetter
 
 def home_screen_view(request):
 	
 	context = {}
-	accounts = Account.objects.all()
-	context['accounts'] = accounts
+	query = ""
+	if request.GET:
+		query = request.GET['q']
+		context['query'] = str(query)
 
+	blog_posts = sorted(BlogPost.objects.all(), key= attrgetter('date_updated'), reverse= True)
+	context['blog_posts'] = blog_posts
+	
 	return render(request, "personal/home.html", context)
