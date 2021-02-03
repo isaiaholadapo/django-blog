74327b1b68a7bb30886dd7e4fe47a45a0b63ec5f
Isaiah <isaiaholadapo18@gmail.com>
2021-02-03 23:06:59 +0100

    set up serializer

--- apiblog/urls.py
+++ apiblog/urls.py
@@ -37,6 +37,7 @@ urlpatterns = [
     path('login/', login_view, name = 'login'),
     path('account/', account_view, name = 'account'),
     path('must_authenticate/', must_authenticate_view, name = 'must_authenticate'),
+    path('api/blog/', include('blog.api.urls', 'blog_api')),
 
     # Password reset links (ref: https://github.com/django/django/blob/master/django/contrib/auth/views.py)
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), 
